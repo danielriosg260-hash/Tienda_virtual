373ac1b70084df0816261a866c062409ca8a1cb5
danielriosg260-hash <danielrios260@gmail.com>
2026-09-01 14:54:55 -0500

    feat: eliminar, editar, crear, listar  productos

---
+++ controllers/productos.controller.js
@@ -0,0 +1,24 @@
+const Producto = require("../models/producto.model");
+
+const listarProductos = async (req, res) => {
+    // ...
+};
+
+const crearProducto = async (req, res) => {
+    // ...
+};
+
+const editarProducto = async (req, res) => {
+    // ...
+};
+
+const eliminarProducto = async (req, res) => {
+    // ...
+};
+
+module.exports = {
+    listarProductos,
+    crearProducto,
+    editarProducto,
+    eliminarProducto
+};
