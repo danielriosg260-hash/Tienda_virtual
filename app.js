40107aa664ad4edf073bf503f3a9a5ec6f5f152c
Marlon <marlonpalaciosmoreno8@gmail.com>
2026-09-01 15:39:13 -0500

    feat: CRUD simple de productos y conexión de rutas

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const conectarDB = require("./config/database");
+const productosRoutes = require("./routes/productos.routes");
 
 dotenv.config();
 
@@ -14,8 +15,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+app.use("/productos", productosRoutes);
+
 app.get("/", (req, res) => {
-    res.send("Tienda virtual funcionando");
+    res.redirect("/productos");
 });
 
 const PORT = process.env.PORT || 3000;
--- controllers/productos.controller.js
+++ controllers/productos.controller.js
@@ -1,24 +1,59 @@
 const Producto = require("../models/producto.model");
 
 const listarProductos = async (req, res) => {
-    // ...
+    try {
+        const productos = await Producto.find({});
+        res.render("productos/listar", { productos });
+    } catch (error) {
+        res.status(500).send("Error al listar productos");
+    }
+};
+
+const formularioNuevo = (req, res) => {
+    res.render("productos/formulario", { producto: null });
 };
 
 const crearProducto = async (req, res) => {
-    // ...
+    try {
+        await Producto.create(req.body);
+        res.redirect("/productos");
+    } catch (error) {
+        res.status(500).send("Error al crear producto");
+    }
+};
+
+const formularioEdicion = async (req, res) => {
+    try {
+        const producto = await Producto.findById(req.params.id);
+        res.render("productos/formulario", { producto });
+    } catch (error) {
+        res.status(500).send("Producto no encontrado");
+    }
 };
 
 const editarProducto = async (req, res) => {
-    // ...
+    try {
+        await Producto.findByIdAndUpdate(req.params.id, req.body);
+        res.redirect("/productos");
+    } catch (error) {
+        res.status(500).send("Error al editar producto");
+    }
 };
 
 const eliminarProducto = async (req, res) => {
-    // ...
+    try {
+        await Producto.findByIdAndDelete(req.params.id);
+        res.redirect("/productos");
+    } catch (error) {
+        res.status(500).send("Error al eliminar producto");
+    }
 };
 
 module.exports = {
     listarProductos,
+    formularioNuevo,
     crearProducto,
+    formularioEdicion,
     editarProducto,
     eliminarProducto
 };
